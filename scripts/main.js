ec2dc4f11c905ef55f51f7daf899889117681604
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-01 14:51:23 -0500

    Some Funcionalities Added

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,52 @@
+function createNode(a_box, data)
+{
+        if (typeof createNode.clicks == 'undefined' || createNode.clicks == 0)
+        {
+                data.style.visibility = "visible";
+                data.style.opacity = "1";
+                createNode.clicks = 1;
+                return;
+        }
+
+        if (createNode.clicks > 0)
+        {
+                node = document.createElement("p");
+                node.appendChild(document.createTextNode(data.value));
+                node.setAttribute("class", "node");
+
+                a_box.appendChild(node);
+
+                createNode.clicks = 0;
+
+                data.value = "";
+                data.style.opacity = "0";
+                data.style.visibility = "hidden";
+        }
+}
+
+function deleteNode(a_box)
+{
+        /*setTimeout(function() {
+                a_box.removeChild(a_box.children[0])
+        }, 500);*/
+
+        a_box.removeChild(a_box.children[0])
+}
+
 window.onload = function ()
 {
 
         add = document.getElementById('add');
         remove = document.getElementById('remove');
         data = document.getElementById('t_box');
+        box = document.getElementById('list');
 
         add.onclick = function(){
-                alert(`You have pressed ${add.type} with name ${add.name}`);
+                createNode(box, data);
         }
 
         remove.onclick = function(){
-                alert(`You have pressed ${remove.type} with name ${remove.name}`);
+                deleteNode(box);
         }
 
-        data.style.display = "none";
 }

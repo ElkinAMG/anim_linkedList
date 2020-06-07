f34ab44909bd249b6dda485322d01e13a053a117
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-06 22:55:13 -0500

    Added Some New Features

--- scripts/main.js
+++ scripts/main.js
@@ -11,26 +11,48 @@ function createNode(a_box, data)
         if (createNode.clicks > 0)
         {
                 node = document.createElement("p");
-                node.appendChild(document.createTextNode(data.value));
-                node.setAttribute("class", "node");
 
-                a_box.appendChild(node);
 
-                createNode.clicks = 0;
+                arrow = document.createElement("div")
+                arrow.setAttribute("id", "arrow")
+                arrow.appendChild(document.createElement("div"))
+                arrow.children[0].setAttribute("id", "stick")
+                arrow.appendChild(document.createElement("div"))
+                arrow.children[1].setAttribute("id", "point")
 
-                data.value = "";
-                data.style.opacity = "0";
-                data.style.visibility = "hidden";
+                if (data.value.length > 0)
+                {
+                        node.appendChild(document.createTextNode(data.value));
+                        node.setAttribute("class", "node");
+
+                        a_box.appendChild(node);
+                        a_box.appendChild(arrow);
+
+                        node.style.opacity = "1";
+                        arrow.style.opacity = "1";
+
+                        createNode.clicks = 0;
+
+                        data.value = "";
+                        data.style.opacity = "0";
+                        data.style.visibility = "hidden";
+                }
+                else
+                {
+                        alert("Please enter a valid value");
+                }
         }
 }
 
 function deleteNode(a_box)
 {
-        /*setTimeout(function() {
-                a_box.removeChild(a_box.children[0])
-        }, 500);*/
+        a_box.children[1].style.opacity = 0;
+        a_box.children[0].style.opacity = 0;
 
-        a_box.removeChild(a_box.children[0])
+        setTimeout(function(){
+                a_box.removeChild(a_box.children[1]);
+                a_box.removeChild(a_box.children[0]);
+        }, 505)
 }
 
 window.onload = function ()
